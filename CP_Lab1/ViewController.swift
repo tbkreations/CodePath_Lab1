6f4d2ed9d37d4c1c0c08293d9481efbac10c4824
tbkreations <trae0714@gmail.com>
2020-02-15 14:04:37 -0500

    Initial Commit

---
+++ CP_Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CP_Lab1
+//
+//  Created by Trae Brown on 2/15/20.
+//  Copyright © 2020 Trae Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CP_Lab1Tests/CP_Lab1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CP_Lab1Tests.swift
+//  CP_Lab1Tests
+//
+//  Created by Trae Brown on 2/15/20.
+//  Copyright © 2020 Trae Brown. All rights reserved.
+//
+
+import XCTest
+@testable import CP_Lab1
+
+class CP_Lab1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
