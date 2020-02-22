9e5660f86965d70e56be3bca9c510060f241ac1e
tbkreations <trae0714@gmail.com>
2020-02-21 20:47:01 -0500

    Final Changes

--- CP_Lab1/ViewController.swift
+++ CP_Lab1/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var a1Label: UILabel!
+    @IBOutlet weak var a2Label: UILabel!
+    @IBOutlet weak var a3Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.lineBreakMode = NSLineBreakMode.byWordWrapping;
+        card.layer.cornerRadius = 20.0;
+        card.clipsToBounds = true;
+        a1Label.layer.cornerRadius = 20.0;
+        a1Label.clipsToBounds = true;
+        a2Label.layer.cornerRadius = 20.0;
+        a2Label.clipsToBounds = true;
+        a3Label.layer.cornerRadius = 20.0;
+        a3Label.clipsToBounds = true;
     }
-
-
+    @IBAction func didTapOnCard(_ sender: Any) {
+          if (questionLabel.isHidden == false) {
+              questionLabel.isHidden = true;
+          } else {
+              questionLabel.isHidden =  false;
+          }
+    }
+    
+    @IBAction func didTapOnA1(_ sender: Any) {
+        a1Label.isHidden = true;
+    }
+    @IBAction func didTapOnA2(_ sender: Any) {
+        a2Label.isHidden = true;
+    }
+    @IBAction func didTapOnA3(_ sender: Any) {
+        a3Label.layer.backgroundColor = UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1).cgColor;
+        questionLabel.isHidden = true;
+    }
+    @IBAction func didTapOnReset(_ sender: Any) {
+        a1Label.isHidden = false;
+        a2Label.isHidden = false;
+        a3Label.isHidden = false;
+        questionLabel.isHidden = false;
+        a3Label.backgroundColor = UIColor(red: 148.0/255, green: 17.0/255, blue: 0/255, alpha: 1);
+    }
 }
 
